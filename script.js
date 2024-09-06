bfb215f1f51b604aef2e641e832611d41374c10f
Vijayrautela <>
2024-09-06 16:52:37 +0530

    nav back

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ themeBtn.addEventListener('click', () => {
 		document.body.classList.add('dark-theme');
 		document.body.classList.remove('light-theme');
         navBar.classList.add('dark-theme');
+		navBar.classList.remove('light-theme');
 		themeIcon.src = 'img/icons8-sun-white.png';
 		currentTheme = 'dark-theme';
 	} else {
 		document.body.classList.add('light-theme');
 		document.body.classList.remove('dark-theme');
+		navBar.classList.add('light-theme');
         navBar.classList.remove('dark-theme');
 		themeIcon.src = 'img/icons8-moon-50.png';
 		currentTheme = 'light-theme';
